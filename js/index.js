6e2cdfe51af29b27507e1bdd671458903576dd86
NikosDigalakis <nikos_23_chania@hotmail.com>
2023-01-14 11:45:11 +0100

    finished index and posts

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const sliderButtonsChangeColor = document.querySelectorAll(".sliderButtons");
 const featuredContainer = document.querySelector(".featured-container");
 const sliderContainer = document.querySelector(".slider-container");
+const slides = document.querySelector(".slides");
 const worldOfWondersURL =
   "https://nikosdigalakis.com/world-of-wonders/wp-json/wp/v2/posts?_embed";
 
@@ -14,12 +15,14 @@ sliderButtonsChangeColor.forEach((target) => {
   });
 });
 
-// api call function for featured items
+// api call function for featured items AND latest posts / category[0] =3 is featured and category[0]=1 is the rest
 async function getPosts(url) {
   const respons = await fetch(url);
   const data = await respons.json();
 
   featuredContainer.innerHTML = ``;
+  slides.innerHTML = ``;
+  let idCounter = 0;
   for (let i = 0; i <= data.length; i++) {
     if (data[i].categories[0] === 3) {
       const cardImg = data[i].better_featured_image.source_url;
@@ -34,6 +37,23 @@ async function getPosts(url) {
         </div>
       </div>
     </a>
+      `;
+    } else {
+      const cardImg = data[i].better_featured_image.source_url;
+      idCounter++;
+      slides.innerHTML += `
+      <div id="slide-${idCounter}">
+      <a href="" title="Read about ${data[i].title.rendered}">
+        <div class="card" style="background: url(${cardImg})no-repeat center">
+          <div class="card-info">
+            <h3 class="card-title">${data[i].title.rendered}</h3>
+            <p class="card-description">
+            ${data[i].excerpt.rendered}
+            </p>
+          </div>
+        </div>
+      </a>
+    </div>
       `;
     }
   }
---
+++ js/posts.js
@@ -0,0 +1,43 @@
+const numberOfPostsHeading = document.querySelector(".numberOfPosts");
+const postsContainer = document.querySelector(".posts-list-container");
+const loadMoreButton = document.querySelector(".loadMore");
+const worldOfWondersURL =
+  "https://nikosdigalakis.com/world-of-wonders/wp-json/wp/v2/posts?_embed";
+const perPage20 = `${worldOfWondersURL}&per_page=20`;
+
+// API call to wp to show the 10 first posts and fixs the post number heading to the number of posts per page
+async function getPosts(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    postsContainer.innerHTML = ``;
+    numberOfPostsHeading.innerHTML = ``;
+    for (let i = 0; i <= data.length; i++) {
+      const cardImg = data[i].better_featured_image.source_url;
+      postsContainer.innerHTML += `
+            <a href="" title="Read about ${data[i].title.rendered}">
+            <div class="card" style="background: url(${cardImg})no-repeat center">
+              <div class="card-info">
+                <h3 class="card-title">${data[i].title.rendered}</h3>
+                <p class="card-description">
+                    ${data[i].excerpt.rendered}
+                </p>
+              </div>
+            </div>
+          </a>
+            `;
+      numberOfPostsHeading.innerHTML = `${data.length} posts`;
+      console.log(data.length);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getPosts(worldOfWondersURL);
+
+// on click it adds the 20 per page parm on the url and updates the number of posts heading to show the total number of posts
+loadMoreButton.addEventListener("click", () => {
+  getPosts(perPage20);
+});
